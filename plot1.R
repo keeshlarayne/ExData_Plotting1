3a69a597af62637f4ea4bf1ebff88b841ff5457c
keeshlarayne <31516282+keeshlarayne@users.noreply.github.com>
2017-09-25 15:27:35 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,9 @@
 require(data.table)
+#read data 
 tab<-fread("./household_power_consumption.txt", na.strings= '?')
+#create date/time column
+tab$date_time<-dmy_hms(paste(tab$Date, tab$Time))
+#subset tab for days required
 tebbs<-tab[grep("2007-02-01|2007-02-02", tab$Date)]
  
 
