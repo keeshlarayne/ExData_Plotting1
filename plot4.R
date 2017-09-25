9cba07dfe54e962be200a90ee2f03b2c46fb39e5
keeshlarayne <31516282+keeshlarayne@users.noreply.github.com>
2017-09-25 15:29:00 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,6 +1,13 @@
 require(data.table)
+require(lubridate)
+
+#read data 
 tab<-fread("./household_power_consumption.txt", na.strings= '?')
-tebbs<-tab[grep("2007-02-01|2007-02-02", tab$Date)]par(mfrow)
+#create date/time column
+tab$date_time<-dmy_hms(paste(tab$Date, tab$Time))
+#subset tab for days required
+tebbs<-tab[grep("2007-02-01|2007-02-02", tab$Date)]
+
 
 png(file="plot4.png")
 par(mfrow= c(2,2))
